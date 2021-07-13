a84b5559d831fc6d0bf8512adf3d8da4430e0046
Wills Chew <chewwl-wm19@student.tarc.edu.my>
2021-07-13 15:23:19 +0800

    Initial commit

---
+++ app/src/main/java/com/roes/navigationdemo/LoginFragment.kt
@@ -0,0 +1,41 @@
+package com.roes.navigationdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import com.roes.navigationdemo.databinding.FragmentLoginBinding
+
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
+
+        binding.btnLogin.setOnClickListener(){
+            val inputUserName: String = binding.tvName.text.toString()
+            val inputPassWord: String = binding.tvPass.text.toString()
+
+            if (inputUserName == "TNR" && inputPassWord == "tnr"){
+                Navigation.findNavController(it).navigate(R.id.action_loginFragment_to_questionFragment)
+            } else {
+                Toast.makeText(context, "Invalid user", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+        return binding.root
+    }
+
+
+}
---
+++ app/src/main/java/com/roes/navigationdemo/Question.kt
@@ -0,0 +1,8 @@
+package com.roes.navigationdemo
+
+import android.widget.RadioButton
+
+data class Question(
+    val text:String,
+    val answers: List<String>)
+
